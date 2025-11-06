91e74539d2f38c2159dea4a041700d232ed04cc8
Nam <24020243@vnu.edu.vn>
2025-11-06 20:27:50 +0700

    Thêm code mới

---
+++ Test/BallTest.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class BallTest {
+
+    @org.junit.jupiter.api.Test
+    void isFallingToGround() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void move() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void randomizeMove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reverseX() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reverseY() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkWallColiision() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void resetBall() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBounds() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void draw() {
+    }
+}
