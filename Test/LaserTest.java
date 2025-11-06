da5df0b9644352e9ec3f9b4cd0b35d5e8fa18d86
Nam <24020243@vnu.edu.vn>
2025-11-06 21:42:11 +0700

    thêm test junit LasserTest

---
+++ Test/LaserTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LaserTest {
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void isOffScreen() {
+    }
+
+    @Test
+    void getBounds() {
+    }
+
+    @Test
+    void isActive() {
+    }
+
+    @Test
+    void setActive() {
+    }
+}
