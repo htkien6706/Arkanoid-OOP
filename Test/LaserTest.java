75c3f360eeb50271c6d189917210ebecaec9dbd3
Nam <24020243@vnu.edu.vn>
2025-11-06 23:35:53 +0700

    sua test lan n

--- Test/LaserTest.java
+++ Test/LaserTest.java
@@ -43,15 +43,6 @@ class LaserTest {
         // Bây giờ laser nên off-screen
         assertTrue(laser.isOffScreen());
     }
-
-    @Test
-    void isActive() {
-        // Mặc định active = true
-        assertTrue(laser.isActive());
-
-        // Kiểm tra sau khi di chuyển vẫn active
-        laser.move();
-        assertTrue(laser.isActive());
-    }
+    
 
 }
